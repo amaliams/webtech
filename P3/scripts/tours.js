98a5662c63fe9db4e025dde4d9400983c6e360fe
espenmarstein <espenms@stud.ntnu.no>
2018-11-10 11:15:51 +0100

    final prod interactiv map, navbar

--- P3/scripts/tours.js
+++ P3/scripts/tours.js
@@ -1,9 +1,39 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+var e = document.getElementById("img1");
+e.onmouseover = function() {
+  document.getElementById('popupImg1').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('popupImg1').style.display = 'none';
 }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+var e = document.getElementById("img2");
+e.onmouseover = function() {
+  document.getElementById('popupImg2').style.display = 'block';
 }
+e.onmouseout = function() {
+  document.getElementById('popupImg2').style.display = 'none';
+}
+
+var e = document.getElementById("parent3");
+e.onmouseover = function() {
+  document.getElementById('popupImg3').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('popupImg3').style.display = 'none';
+}
+
+var e = document.getElementById("parent4");
+e.onmouseover = function() {
+  document.getElementById('popupImg4').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('popupImg4').style.display = 'none';
+}
+
+var e = document.getElementById("parent5");
+e.onmouseover = function() {
+    document.getElementById('popupImg5').style.display = 'block';
+  }
+  e.onmouseout = function() {
+    document.getElementById('popupImg5').style.display = 'none';
+  }
